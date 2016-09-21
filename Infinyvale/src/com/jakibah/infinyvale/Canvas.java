1fea3743e924579e6b0e81ccbe1b462a92447d0a
Jakibah <iakovospetrouentertainment@gmail.com>
2016-09-21 18:07:10 +0200

    Indev 0.1.1
    Added Drawing method
    Added test32 texture

--- Infinyvale/src/com/jakibah/infinyvale/Canvas.java
+++ Infinyvale/src/com/jakibah/infinyvale/Canvas.java
@@ -1,31 +1,87 @@
 package com.jakibah.infinyvale;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.ResourceLoader;
+
+import static org.lwjgl.opengl.GL11.*;
 
 public class Canvas {
 
-	public static void CreateCanvas(int width, int height, String title){
+	public static void CreateCanvas(int width, int height, String title, int fps) {
+
 		try {
 			Display.setDisplayMode(new DisplayMode(width, height));
-		    Display.setTitle(title);
-		    Display.create();
-		    
-		
+			Display.setTitle(title);
+			Display.create();
+
 		} catch (LWJGLException e) {
 			e.printStackTrace();
-		    Display.destroy();
-		    System.exit(1);
+			Display.destroy();
+			System.exit(1);
 		}
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, width, height, 0, 1, -1);
+		glMatrixMode(GL_MODELVIEW);
+		glEnable(GL_TEXTURE_2D);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 		Game.Start();
-		while(!Display.isCloseRequested()) {
+		while (!Display.isCloseRequested()) {
 			Display.update();
-			Display.sync(60);
+			Display.sync(fps);
 			Game.Update();
 		}
 		Game.Stop();
 		Display.destroy();
 		System.exit(0);
+	}
+
+	public static void DrawQuadTex(Texture tex, float x, float y, float width,
+			float height) {
+		tex.bind();
+		glTranslatef(x, y, 0);
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+		glBegin(GL_QUADS);
+		glTexCoord2f(0, 0);
+		glVertex2f(0, 0);
+		glTexCoord2f(1, 0);
+		glVertex2f(width, 0);
+		glTexCoord2f(1, 1);
+		glVertex2f(width, height);
+		glTexCoord2f(0, 1);
+		glVertex2f(0, height);
+		glEnd();
+		glLoadIdentity();
+
+	}
+
+	public static Texture loadTexture(String path, String fileType) {
+		Texture tex = null;
+		InputStream in = ResourceLoader.getResourceAsStream(path);
+		try {
+			tex = TextureLoader.getTexture(fileType, in);
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		return tex;
+	}
+
+	public static Texture QuickLoad(String name) {
+		Texture tex = null;
+		tex = loadTexture("com/jakibah/infinyvale/resources/textures/" + name
+				+ ".png", "PNG");
+		return tex;
+
 	}
 }
--- Infinyvale/src/com/jakibah/infinyvale/Game.java
+++ Infinyvale/src/com/jakibah/infinyvale/Game.java
@@ -3,23 +3,22 @@ package com.jakibah.infinyvale;
 public class Game {
 
 	public static void main(String[] args) {
-		Canvas.CreateCanvas(640, 480, "Infinyvale");
+		Canvas.CreateCanvas(640, 480, "Infinyvale", 60);
 
 	}
 
 	public static void Start() {
 		System.out.println("Starting...");
-		
+
 	}
 
 	public static void Update() {
-		
 		
 	}
 
 	public static void Stop() {
 		System.out.println("Stopping...");
-		
+
 	}
 
 }
